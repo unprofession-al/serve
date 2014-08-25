081e56c42f288f15fb70f3d2739ba54f7f89ee2d
Daniel Menet <membership@sontags.ch>
2014-08-25 18:58:03 +0200

    enabled logging

--- serve.go
+++ serve.go
@@ -6,13 +6,21 @@ import (
 	"os"
 )
 
+func Log(handler http.Handler, logging string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if logging != "" {
+			log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8989"
 	}
-
+	logging := os.Getenv("LOG")
 	log.Println("listening on port", port)
-
-	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("."))))
+	log.Fatal(http.ListenAndServe(":"+port, Log(http.FileServer(http.Dir(".")), logging)))
 }
